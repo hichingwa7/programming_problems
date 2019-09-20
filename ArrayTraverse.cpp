c5f27a4e7c1156b5517071c411baf9719a1cb577
hichingwa7 <hichingwa7@gmail.com>
2019-09-19 23:18:06 -0700

    implementation of array traversing

---
+++ ArrayTraverse.cpp
@@ -0,0 +1,9 @@
+class ArrayTraverse {
+ArrayTraverse(); //constructor
+void add(double y); //adds elements
+double maximum(); //finds largest elements
+double minimum(); //finds smallest elements
+double average(); //computes average of elements
+void disp(); //prints array elements
+~ ArrayTraverse(); //destructor
+};
