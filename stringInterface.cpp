8c61315edff56a91558ff899a34b5ba847c16d79
hichingwa7 <hichingwa7@gmail.com>
2019-10-13 21:33:25 -0700

    string interface implementation

---
+++ stringInterface.cpp
@@ -0,0 +1,10 @@
+const int size = 20;
+const int ssize = 15;
+class stringInterface {
+private:
+    char x[size][ssize];
+public:
+    stringInterface() { //constructor
+        void add(char s[]);
+    }
+};
