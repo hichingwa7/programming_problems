f308eec2df9eeb1de5987cff4e30310fa9f3a54c
hichingwa7 <hichingwa7@gmail.com>
2019-10-13 21:29:32 -0700

    implementation of array operations

--- Array.cpp
+++ Array.cpp
@@ -1,15 +1,15 @@
 const int size = 30;
 class Array {
 private:
-double x[size];
+    double x[size];
 public:
-Array(); //constructor
-void add(double y); //add method
-void del(); //delete method
-void insert(double p, double y); //insert method
-int getindex(double p); //get method
-bool isFull(); //is array full
-bool isEmpty(); //is array empty
-disp(); //print method
-~Array(); //destructor
+    Array(); //constructor
+    void add(double y); //add method
+    void del(); //delete method
+    void insert(double p, double y); //insert method
+    int getindex(double p); //get method
+    bool isFull(); //is array full
+    bool isEmpty(); //is array empty
+    disp(); //print method
+    ~Array(); //destructor
 };
